81b4558601b138b7823a06365f5692acfe7889c2
shameljij <shamel.jij@gmail.com>
2021-12-10 09:34:26 +0100

    class Car attributes added

--- src/car/Car.java
+++ src/car/Car.java
@@ -1,5 +1,20 @@
 package car;
 
 public class Car {
+    //Atribut die Farbe beschreibt
     String color;
+    //Atribut die Name von Auto Marke. String ist ein Variablen Typ - Beispiel : "jfsodfksnj"
+    String manufacturer;
+    //ob das Auto funktioniert
+    boolean driving;
+    //ob das Auto kaputt ist
+    boolean broken;
+    //int ist ein VariablenTyp Beispiel: 1 223 4334 3423
+    int horsePower;
+    String type;
+    String year;
+    String owner;
+
+
 }
+
