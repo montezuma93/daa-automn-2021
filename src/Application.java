1c9609ca66acc74423f6638d4e985558d4772e23
RM-GBochkarev <gleb.bochkarev@yahoo.com>
2021-12-10 12:23:44 +0100

    Methode bearbeitet

--- src/Application.java
+++ src/Application.java
@@ -2,11 +2,14 @@ import car.Car;
 
 public class Application {
 
-        public static void main(String args[])
-        {
-            Car myCar = new Car("Surf Green", "Audi");
+        public static void main(String args[]) throws Exception {
+            Car myCar = new Car("Surf Green", "Audi", true, false, 150,
+                    "Cabrio", "Enes", 2010, 2, 4, 250,11.2);
             System.out.println(myCar.color);
             System.out.println(myCar.getManufacturer());
+            System.out.println(myCar.speedCheck("km/h"));
+            System.out.println(myCar.speedCheck("mp/h"));
+            System.out.println(myCar.speedCheck("lol"));
         }
 
 
