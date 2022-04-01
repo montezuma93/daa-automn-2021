1abaaae66b609d4622131fbb27eed53fb4ed9645
montezuma93 <hmr@riki5.de>
2022-04-01 12:09:44 +0200

    Adjust Comment

--- src/aufgabe010422/Customer.java
+++ src/aufgabe010422/Customer.java
@@ -1,15 +1,18 @@
 package aufgabe010422;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
     private String firstName;
     private String lastName;
     private int accountNumber;
     private double money;
     private double limit;
-
-
+    private List<String> accountHistory;
 
     public Customer(String firstName, String lastName, int accountNumber, double limit) {
+        this.accountHistory = new ArrayList<>();
         this.firstName=firstName;
         this.lastName=lastName;
         this.accountNumber=accountNumber;
@@ -21,13 +24,21 @@ public class Customer {
             System.out.println("Not enough Money");
         }else {
             money = money - moneyToTake;
-            //money=1000000;
+            accountHistory.add("withdraw "+ moneyToTake +"/ Account Balance: " + money);
+        }
+    }
+
+    public void printAccountStatement(){
+
+        for(String entry:accountHistory){
+            System.out.println(entry);
         }
     }
 
 
     public void deposit(double moneyToAdd){
         money=money+moneyToAdd;
+        accountHistory.add("deposit "+ moneyToAdd +"/ Account Balance: " + money);
     }
 
     public double getMoney() {
